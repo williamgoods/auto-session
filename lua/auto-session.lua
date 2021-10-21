b6bf4cb9cb635cef8630c969d6955cc206a31d5d
williamgoods <m15088326716@163.com>
2021-10-21 21:22:36 +0800

    simplify code

--- lua/auto-session.lua
+++ lua/auto-session.lua
@@ -203,66 +203,66 @@ function AutoSession.AutoSaveSession(sessions_dir)
 	end
 	AutoSession.SaveSession(sessions_dir, true)
 
-	local speed_dir = vim.env.HOME .. "/.vim/neovim_speed" .. vim.fn.getcwd()
-	local dir_ok, _ = SelfLib:exists(speed_dir)
-	-- print(speed_dir)
-	if not dir_ok then
-		os.execute("mkdir -p " .. speed_dir)
-	end
-
-	local speed_file = speed_dir .. "/speed"
-	local file_ok, _ = SelfLib:exists(speed_file)
-
-	if not file_ok then
-		os.execute("touch " .. speed_file)
-	end
-
-	local buffers_size = 0
-	local all_buffers = vim.api.nvim_list_bufs()
-	for _, value in ipairs(all_buffers) do
-		-- print("key: " .. key .. ", value: " .. value .. "\n")
-		-- print("lines: " .. tostring(vim.api.nvim_buf_line_count(value)) .. "\n")
-		buffers_size = buffers_size + vim.api.nvim_buf_line_count(value)
-	end
-
-	local buffers_size_k = (buffers_size - buffers_size % 1000) / 1000 + 1
-	-- print("buffers_size: " .. buffers_size_k .. "k")
-
-	--{
-	--  first = 1(default: 1, after first load, this will be 0),
-	--  previous_speed = number(default: 500ms),
-	--  buffers_size = size,
-	--}
-
-	local speed_config = ""
-
-	SelfLib:FileOpration(speed_file, "r", function (filehandler)
-		speed_config = filehandler:read()
-	end)
-
-	if not speed_config then
-		local speed = {
-			first = 1,
-			previous_speed = 500,
-			buffers_size = 1,
-		}
-
-		local speed_json = vim.fn.json_encode(speed)
-
-		SelfLib:FileOpration(speed_file, "w", function (filehandler)
-			filehandler:write(speed_json)
-		end)
-	else
-		local speed_decode = vim.fn.json_decode(speed_config)
-
-		speed_decode['buffers_size'] = buffers_size_k
-
-		local re_speed_config = vim.fn.json_encode(speed_decode)
-
-		SelfLib:FileOpration(speed_file, "w", function (filehandler)
-			filehandler:write(re_speed_config)
-		end)
-	end
+	-- local speed_dir = vim.env.HOME .. "/.vim/neovim_speed" .. vim.fn.getcwd()
+	-- local dir_ok, _ = SelfLib:exists(speed_dir)
+	-- -- print(speed_dir)
+	-- if not dir_ok then
+		-- os.execute("mkdir -p " .. speed_dir)
+	-- end
+--
+	-- local speed_file = speed_dir .. "/speed"
+	-- local file_ok, _ = SelfLib:exists(speed_file)
+--
+	-- if not file_ok then
+		-- os.execute("touch " .. speed_file)
+	-- end
+--
+	-- local buffers_size = 0
+	-- local all_buffers = vim.api.nvim_list_bufs()
+	-- for _, value in ipairs(all_buffers) do
+		-- -- print("key: " .. key .. ", value: " .. value .. "\n")
+		-- -- print("lines: " .. tostring(vim.api.nvim_buf_line_count(value)) .. "\n")
+		-- buffers_size = buffers_size + vim.api.nvim_buf_line_count(value)
+	-- end
+--
+	-- local buffers_size_k = (buffers_size - buffers_size % 1000) / 1000 + 1
+	-- -- print("buffers_size: " .. buffers_size_k .. "k")
+--
+	-- --{
+	-- --  first = 1(default: 1, after first load, this will be 0),
+	-- --  previous_speed = number(default: 500ms),
+	-- --  buffers_size = size,
+	-- --}
+--
+	-- local speed_config = ""
+--
+	-- SelfLib:FileOpration(speed_file, "r", function (filehandler)
+		-- speed_config = filehandler:read()
+	-- end)
+--
+	-- if not speed_config then
+		-- local speed = {
+			-- first = 1,
+			-- previous_speed = 500,
+			-- buffers_size = 1,
+		-- }
+--
+		-- local speed_json = vim.fn.json_encode(speed)
+--
+		-- SelfLib:FileOpration(speed_file, "w", function (filehandler)
+			-- filehandler:write(speed_json)
+		-- end)
+	-- else
+		-- local speed_decode = vim.fn.json_decode(speed_config)
+--
+		-- speed_decode['buffers_size'] = buffers_size_k
+--
+		-- local re_speed_config = vim.fn.json_encode(speed_decode)
+--
+		-- SelfLib:FileOpration(speed_file, "w", function (filehandler)
+			-- filehandler:write(re_speed_config)
+		-- end)
+	-- end
   end
 end
 
@@ -305,6 +305,8 @@ function AutoSession.SaveSession(sessions_dir, auto)
 
   local post_cmds = AutoSession.get_cmds("post_save")
   run_hook_cmds(post_cmds, "post-save")
+
+	SelfLib:SaveSpeed()
 end
 
 -- This function avoids calling RestoreSession automatically when argv is not nil.
@@ -318,26 +320,27 @@ function AutoSession.AutoRestoreSession(sessions_dir)
 
 		local interval = current_end - curret_start
 
-		local speed_config = ""
-		local speed_dir = vim.env.HOME .. "/.vim/neovim_speed" .. vim.fn.getcwd()
-		local speed_file = speed_dir .. "/speed"
-
-		SelfLib:FileOpration(speed_file, "r", function (filehandler)
-			speed_config = filehandler:read()
-		end)
-
-		local speed_json = vim.fn.json_decode(speed_config)
-
-		local previous_buffers_size = speed_json["buffers_size"]
-		local current_speed = interval / previous_buffers_size
-
-		speed_json["previous_speed"] = current_speed
-
-		SelfLib:FileOpration(speed_file, "w", function (filehandler)
-			filehandler:write(vim.fn.json_encode(speed_json))
-		end)
+		SelfLib:RestoreSpeed(interval)
 
 		vim.api.nvim_command("LspStop")
+		-- local speed_config = ""
+		-- local speed_dir = vim.env.HOME .. "/.vim/neovim_speed" .. vim.fn.getcwd()
+		-- local speed_file = speed_dir .. "/speed"
+--
+		-- SelfLib:FileOpration(speed_file, "r", function (filehandler)
+			-- speed_config = filehandler:read()
+		-- end)
+--
+		-- local speed_json = vim.fn.json_decode(speed_config)
+--
+		-- local previous_buffers_size = speed_json["buffers_size"]
+		-- local current_speed = interval / previous_buffers_size
+--
+		-- speed_json["previous_speed"] = current_speed
+--
+		-- SelfLib:FileOpration(speed_file, "w", function (filehandler)
+			-- filehandler:write(vim.fn.json_encode(speed_json))
+		-- end)
   end
 end
 
--- lua/self-session-library.lua
+++ lua/self-session-library.lua
@@ -32,4 +32,88 @@ function self_session_library:FileOpration(filename, operation, filefunc)
 	file:close()
 end
 
+function self_session_library:RestoreSpeed(interval)
+		local speed_config = ""
+		local speed_dir = vim.env.HOME .. "/.vim/neovim_speed" .. vim.fn.getcwd()
+		local speed_file = speed_dir .. "/speed"
+
+		self:FileOpration(speed_file, "r", function (filehandler)
+			speed_config = filehandler:read()
+		end)
+
+		local speed_json = vim.fn.json_decode(speed_config)
+
+		local previous_buffers_size = speed_json["buffers_size"]
+		local current_speed = interval / previous_buffers_size
+
+		speed_json["previous_speed"] = current_speed
+
+		self:FileOpration(speed_file, "w", function (filehandler)
+			filehandler:write(vim.fn.json_encode(speed_json))
+		end)
+end
+
+function self_session_library:SaveSpeed()
+	local speed_dir = vim.env.HOME .. "/.vim/neovim_speed" .. vim.fn.getcwd()
+	local dir_ok, _ = self:exists(speed_dir)
+	-- print(speed_dir)
+	if not dir_ok then
+		os.execute("mkdir -p " .. speed_dir)
+	end
+
+	local speed_file = speed_dir .. "/speed"
+	local file_ok, _ = self:exists(speed_file)
+
+	if not file_ok then
+		os.execute("touch " .. speed_file)
+	end
+
+	local buffers_size = 0
+	local all_buffers = vim.api.nvim_list_bufs()
+	for _, value in ipairs(all_buffers) do
+		-- print("key: " .. key .. ", value: " .. value .. "\n")
+		-- print("lines: " .. tostring(vim.api.nvim_buf_line_count(value)) .. "\n")
+		buffers_size = buffers_size + vim.api.nvim_buf_line_count(value)
+	end
+
+	local buffers_size_k = (buffers_size - buffers_size % 1000) / 1000 + 1
+	-- print("buffers_size: " .. buffers_size_k .. "k")
+
+	--{
+	--  first = 1(default: 1, after first load, this will be 0),
+	--  previous_speed = number(default: 500ms),
+	--  buffers_size = size,
+	--}
+
+	local speed_config = ""
+
+	self:FileOpration(speed_file, "r", function (filehandler)
+		speed_config = filehandler:read()
+	end)
+
+	if not speed_config then
+		local speed = {
+			first = 1,
+			previous_speed = 500,
+			buffers_size = 1,
+		}
+
+		local speed_json = vim.fn.json_encode(speed)
+
+		self:FileOpration(speed_file, "w", function (filehandler)
+			filehandler:write(speed_json)
+		end)
+	else
+		local speed_decode = vim.fn.json_decode(speed_config)
+
+		speed_decode['buffers_size'] = buffers_size_k
+
+		local re_speed_config = vim.fn.json_encode(speed_decode)
+
+		self:FileOpration(speed_file, "w", function (filehandler)
+			filehandler:write(re_speed_config)
+		end)
+	end
+end
+
 return self_session_library
